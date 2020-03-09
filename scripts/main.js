23b991e23b5f55d84e65bfbb5bf12e010a2f8b69
LukeGiles3 <lukegiles@Lukes-MacBook-Pro.local>
2020-03-09 10:56:21 -0600

    space

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 }); 
 
 function getMovies(searchText){
-    axios.get('http://www.omdbapi.com/?s=' +searchText + '&apikey=632765f7')
+    axios.get('http://www.omdbapi.com/?s=' + searchText + '&apikey=632765f7')
         .then((response) => {
             console.log(response);
             let movies = response.data.Search;
